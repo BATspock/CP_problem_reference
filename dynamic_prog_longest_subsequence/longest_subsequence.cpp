cd52213394d0e6437e6673a9c8316ea405d1b934
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2021-05-11 10:54:20 +0530

    created skeleton need to add function

---
+++ dynamic_prog_longest_subsequence/longest_subsequence.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int longest_subsequence(vector<int> v){
+    
+}
+
+int main(){
+    vector<int> v;
+    int arr[] = {6,2,5,1,7,4,8,3};
+    int N = sizeof(arr)/sizeof(arr[0]);
+    for (int i =0;i < N;i++) v.push_back(arr[i]);
+
+    cout<< longest_subsequence(v)<<"\n";
+}
