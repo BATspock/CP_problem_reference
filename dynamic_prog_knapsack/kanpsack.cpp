488175baf8b85eb9026716370456ee4886f928b6
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2021-05-19 22:07:54 +0530

    added knapsack code with single array

--- dynamic_prog_knapsack/kanpsack.cpp
+++ dynamic_prog_knapsack/kanpsack.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 void kanpsack(vector<int> a){
-
+    int totalwt = accumulate(a.begin(), a.end(),0);
+    vector<int> possible;
+    possible.assign(totalwt+1,0);
+    possible[0] = 1;
+    for (int k = 0;k<a.size();k++){
+        for (int i = totalwt; i >=0;i--){
+            if (possible[i]) possible[i + a[k]] = 1;
+        }
+    }
+    for (int i =0;i < possible.size();i++){
+        if (possible[i] == 1) cout<<i<<endl;
+    }
 }
 
 int main(){
