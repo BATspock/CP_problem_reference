92134cd44748a1bf1bd8979991e599d6fc4a3c0c
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2021-05-29 16:28:48 +0530

    optimized but still needs to check for overflow

--- counting_subgrids/optimizedsubgrid.cpp
+++ counting_subgrids/optimizedsubgrid.cpp
@@ -2,39 +2,39 @@
 
 using namespace std;
 
+int convertonumber(valarray<int> a){
+    int num = 0;
+    for (int i = 0;i<a.size();i++) num+= pow(2, a.size()-i-1)*a[i];
+    return num;
+}
+
 int countsubgrids(vector<vector<int>> v, int n, int N){
-    int subgrids = 0;
-    int steps = n/N;
-    int flag = n%N;
+    int subgrids = 0, steps = n/N, flag = n%N, val;
 
-    for (int r1 =0;r1 < n; r1++){
-        int hit = 0;
+    for (int r1 =0;r1 < n; r1++){        
         for (int r2 =r1+1;r2<n;r2++){
-            int i=0;
-            for (i;i<n;i+=N){
-                vector<int> x = vector<int>(v[r1].begin()+i ,v[r1].begin()+i+N);
+            
+            int hit = 0;
+            
+            for (int i=0;i<n;i+=N){
                 
-                vector<int> y = vector<int>(v[r2].begin()+i ,v[r2].begin()+i+ N);
+                vector<int> x(N, 0);
+                vector<int> y(N, 0);
 
-                valarray<int> test1(x.data(), x.size());
-                valarray<int> test2(y.data(), y.size());
+                x.assign(v[r1].begin()+i ,v[r1].begin()+i+N);                
+                y.assign(v[r2].begin()+i ,v[r2].begin()+i+N);
                 
-                hit =__builtin_popcount(test1&test2);
-                }
-            if (flag){
-                vector<int> x = vector<int>(v[r1].begin() + i ,v[r1].begin() + i + flag);
+                valarray<int> test1(x.data(), x.size()); 
+                valarray<int> test2(y.data(), y.size()); 
                 
-                vector<int> y = vector<int>(v[r2].begin() + i ,v[r2].begin() + i + flag);
-
-                valarray<int> test1(x.data(), x.size());
-                valarray<int> test2(y.data(), y.size());
+                val = convertonumber(test1 & test2);
                 
-                hit =__builtin_popcount(test1&test2);
-                }
-
-            }        
-            subgrids+=((hit*(hit-1))/2);
-        } 
+                hit +=__builtin_popcount(val); 
+            }
+            subgrids+=((hit*(hit-1))/2);         
+        }        
+            
+    } 
     return subgrids;
 }
 
@@ -42,15 +42,7 @@ int main(){
 
     vector<vector<int>> vec{{0,1,0,0,1}, {0,1,1,0,0},{1,0,0,0,0},{0,1,1,0,1},{0,0,0,0,0}};
     int N = 5;
-    int s = 2;
+    int s = 3;
     cout<< countsubgrids(vec,N, s)<<endl;    
-    //for ( auto e: x) cout<<e;
-    //vector<int> x = vector<int>(vec[0].begin() + 1, vec[0].begin() + 4);
-    //vector<int> y = vector<int>(vec[1].begin() + 1, vec[1].begin() + 4);
-    //valarray<int> test1(x.data(), x.size());
-    //valarray<int> test2(y.data(), y.size());
-    //valarray<int> c = test1&test2;
-
-    //for (auto e : c) cout<<e<<endl;
     return 0;
 }
