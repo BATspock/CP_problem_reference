4f23580a4febb633979ac254ca4e68acb0e15069
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2021-06-16 23:13:47 +0530

    using next_permutations

---
+++ backtracking/generate_permutations.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Permutations{
+    public:
+
+        vector<vector<int>> permute(vector<int> &nums);
+};
+
+vector<vector<int>> Permutations ::  permute(vector<int> &nums){
+    vector<vector<int>> r;
+    sort(nums.begin(), nums.end());
+    do {
+        r.push_back(nums);
+
+    }while(next_permutation(nums.begin(), nums.end()));
+
+    return r;
+}
+
+int main(){
+
+    vector<int> nums{1, 2, 3};
+    vector<vector<int>> ans;
+
+    Permutations *obj = new Permutations();
+    ans = obj->permute(nums);
+    cout<<"[";
+    for (int i = 0; i<ans.size();i++){
+        cout<<"[";
+        for (int j = 0; j<ans[0].size();j++){
+            cout<< ans[i][j]<<" ";
+        }
+        cout<<"]";
+    } 
+    cout<<"]";
+
+    return 0;
+}
