36cfa1ec16cef8e5bf735f8c558db973e0831b2f
BATspock <30798316+BATspock@users.noreply.github.com>
2021-08-03 23:19:26 +0530

    heapify

--- Heap/heapify.cpp
+++ Heap/heapify.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
 
+class Heapify{
+
+    public:
+    std::vector<int> heapify(std::vector<int> arr){
+        std:: vector<int> ans;
+
+        //complete heapify bottom up 
+
+
+    }
+};
+
+int main(){
+    std::vector<int> arr {4, 6, 2, 4, 1, 7, 10, 5};
+
+    for (auto c: arr){
+        std::cout<<c<<'\t';
+    }
+
+    return 0;
+}
