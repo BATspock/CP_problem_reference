542a47f9c6cb329c0f396d4d3dd7de51144e42c8
BATspock <30798316+BATspock@users.noreply.github.com>
2021-08-04 20:53:39 +0530

    heapify algorithm

--- Heap/heapify.cpp
+++ Heap/heapify.cpp
@@ -8,6 +8,7 @@ class Heapify{
 
         //complete heapify bottom up 
 
+    return ans;
 
     }
 };
@@ -15,7 +16,18 @@ class Heapify{
 int main(){
     std::vector<int> arr {4, 6, 2, 4, 1, 7, 10, 5};
 
+
+    std::cout<<"\nBefore heapify: \n";
     for (auto c: arr){
+        std::cout<<c<<'\t';
+    }
+
+    Heapify obj;
+    std::vector<int> a = obj.heapify(arr);
+
+    std::cout<<"\nAfter heapify: \n";
+
+    for (auto c: a){
         std::cout<<c<<'\t';
     }
 
