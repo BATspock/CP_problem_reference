7d8ab2c2cd62f02f16f1330b69624edfe6fbd0a7
BATspock <30798316+BATspock@users.noreply.github.com>
2021-08-09 21:39:38 +0530

    using this pointer

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -1,6 +1,24 @@
 #include<bits/stdc++.h>
 #include "heapify.hpp"
 
+class HeapSort{
+
+public:
+    std::vector<int> ar;
+
+    HeapSort(std::vector<int> a){
+        ar.assign(a.begin(), a.end());
+    }
+
+    void print_array(){
+        for (auto c: this->ar){
+            std::cout<<c<<'\t';
+        }
+        std::cout<<'\n';
+    }
+};
+
+
 int main(){
 
     std::vector<int> arr {4, 6, 2, 4, 1, 7, 10, 5};
@@ -13,6 +31,13 @@ int main(){
     for (auto c: a){
         std::cout<<c<<'\t';
     }
+    std::cout<<'\n';
+    HeapSort obj2(a);
+    HeapSort obj3(arr);
+
+    obj2.print_array();
+    obj3.print_array();
+
 
     return 0;
 }
