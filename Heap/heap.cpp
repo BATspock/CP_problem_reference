11e5f44c30c625b368bbcb1fbde13155e5c64b34
BATspock <30798316+BATspock@users.noreply.github.com>
2021-08-10 17:28:36 +0530

    heap sort and priority queue

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -17,15 +17,21 @@ public:
         std::cout<<'\n';
     }
 
-    std::vector<int> hapifyarray(){
+    std::vector<int> heapifyarray(){
         Heapify obj;
-        return obj.heapify(this->ar);
+        this->ar =  obj.heapify(this->ar);
+        return this->ar;
     }
 
     int delete_top_and_return(){
 
-        int var = this->ar.front()
-        this->ar.erase(this->ar.begin());
+        int var = this->ar.front();
+        int last = this->ar[this->ar.size()-1];
+        this->ar[0] = last;
+        this->ar.erase(this->ar.begin()+(this->ar.size()-1));
+        this->print_array();
+        Heapify obj;
+        this->ar = obj.heapify(this->ar);
         return var;
 
     }
@@ -34,22 +40,21 @@ public:
 
 int main(){
 
-    std::vector<int> arr {4, 6, 2, 4, 1, 7, 10, 5};
+    std::vector<int> arr {4, 6, 2, 14, 1, 7, 10, 5};
 
+    HeapSort obj(arr);
     
 
-    std::cout<<"\nAfter heapify: \n";
+    std::vector<int> a = obj.heapifyarray();
 
-    for (auto c: a){
-        std::cout<<c<<'\t';
+    obj.print_array();
+
+    for (int i = 0;i<arr.size();i++){
+        std::cout<<"iteration: "<<i+1<<'\t'<<"array: ";
+        int junk = obj.delete_top_and_return();
+        obj.print_array();
+        //std::cout<<junk<<'\n';
     }
-    std::cout<<'\n';
-    HeapSort obj2(a);
-    HeapSort obj3(arr);
-
-    obj2.print_array();
-    obj3.print_array();
-
-
+    
     return 0;
 }
