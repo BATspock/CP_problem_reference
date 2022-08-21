7f5ea1573bfb8c34fe19b15adb3d19b98a84384e
Aditya Kishore <aditya20kishore@gmail.com>
2022-08-20 20:07:35 -0700

    updated amazonoa1

--- PlacementPractice/amazonoa1.cpp
+++ PlacementPractice/amazonoa1.cpp
@@ -6,6 +6,9 @@ using namespace std;
 class Solution {
 public:
 	int team_size(vector<int>& v) {
+
+		if (v.size()==0) return 0;
+		if (v.size()==2) return 1;
 		sort(v.begin(), v.end());
 		unordered_set<float> s;
 		for (int i = 0; i < v.size() / 2 - 1; i++) {
@@ -19,7 +22,7 @@ public:
 
 int main() {
 
-	vector<int> v{ 1,4,1,3,5,6 };
+	vector<int> v{ 100,10};
 	Solution *object1 = new Solution();
 	cout << object1->team_size(v);
 	return 0;
