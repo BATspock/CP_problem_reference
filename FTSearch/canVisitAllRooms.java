554be8390b33aaeec52cc35a18f49ee4dc6fa620
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2023-06-17 21:51:10 -0700

    Update canVisitAllRooms.java

--- FTSearch/canVisitAllRooms.java
+++ FTSearch/canVisitAllRooms.java
@@ -6,3 +6,39 @@ When you visit a room, you may find a set of distinct keys in it. Each key has a
 
 Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, or false otherwise.
 */
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms.size();
+        int[] visited = new int[n];
+        Arrays.fill(visited,0);
+        Stack<Integer> stack = new Stack<>();
+        visited[0] = 1;
+
+        for (int i =0;i<rooms.get(0).size();i++){
+
+                stack.push(rooms.get(0).get(i));   
+        }
+
+        while(!stack.isEmpty()){
+            int top = stack.pop();
+            visited[top] = 1;
+
+            for (int i =0;i<rooms.get(top).size();i++){
+                if (visited[rooms.get(top).get(i)] == 0){
+                    stack.push(rooms.get(top).get(i));
+                }
+            }
+            
+        }
+        
+        for (int x: visited){
+            if (x == 0){
+                return false;
+            }
+        }
+
+        return true;
+
+
+    }
+}
