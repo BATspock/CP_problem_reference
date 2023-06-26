2f8b9f71bfbb36e7bf84be4eafb87499a9f133a3
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2023-06-25 21:59:30 -0700

    Create leetcode586.sql

---
+++ FTSearch/SQLPractice/leetcode586.sql
@@ -0,0 +1,21 @@
+-- Table: Orders
+
+-- +-----------------+----------+
+-- | Column Name     | Type     |
+-- +-----------------+----------+
+-- | order_number    | int      |
+-- | customer_number | int      |
+-- +-----------------+----------+
+-- order_number is the primary key for this table.
+-- This table contains information about the order ID and the customer ID.
+ 
+
+-- Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+
+-- The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+
+
+SELECT customer_number 
+FROM Orders
+group by customer_number 
+order by count(order_number) desc limit 1; 
