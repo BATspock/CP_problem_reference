d0ae973a5353693a254657519b7159a9dfdc99b4
BATspock <aditya20kishore@gmail.com>
2023-07-10 23:13:49 -0700

    solution for leetcode 1407

--- FTSearch/SQLPractice/Top Travellers.sql
+++ FTSearch/SQLPractice/Top Travellers.sql
@@ -1,1 +1,12 @@
-
+select U.name, case 
+                when sum(R.distance) is null
+                then 0
+                when sum(R.distance) is not null
+                then sum(R.distance)
+                end
+                as travelled_distance
+from Users U
+left join Rides R
+on U.id = R.user_id
+group by R.user_id
+order by sum(R.distance) desc, name;
