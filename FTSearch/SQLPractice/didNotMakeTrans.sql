9659a2bf071252a4b97d2d5270e5726a50fef080
BATspock <aditya20kishore@gmail.com>
2023-07-19 00:55:36 -0700

    subquery for did not make trans.sql

--- FTSearch/SQLPractice/didNotMakeTrans.sql
+++ FTSearch/SQLPractice/didNotMakeTrans.sql
@@ -71,3 +71,17 @@ Customer with id = 30 visited the mall once and did not make any transactions.
 Customer with id = 54 visited the mall three times. During 2 visits they did not make any transactions, and during one visit they made 3 transactions.
 Customer with id = 96 visited the mall once and did not make any transactions.
 As we can see, users with IDs 30 and 96 visited the mall one time without making any transactions. Also, user 54 visited the mall twice and did not make any transactions.
+
+
+
+select  customer_id, count(visit_id) as count_no_trans
+
+from Visits 
+
+where visit_id 
+
+not in (
+  select visit_id from Transactions
+)
+
+group by customer_id
