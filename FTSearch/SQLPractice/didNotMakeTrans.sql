25ba4d91291b03a906d1c0f26cd3186e5db3e411
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2023-07-19 13:18:44 +0530

    Update didNotMakeTrans.sql

--- FTSearch/SQLPractice/didNotMakeTrans.sql
+++ FTSearch/SQLPractice/didNotMakeTrans.sql
@@ -71,3 +71,10 @@ Customer with id = 30 visited the mall once and did not make any transactions.
 Customer with id = 54 visited the mall three times. During 2 visits they did not make any transactions, and during one visit they made 3 transactions.
 Customer with id = 96 visited the mall once and did not make any transactions.
 As we can see, users with IDs 30 and 96 visited the mall one time without making any transactions. Also, user 54 visited the mall twice and did not make any transactions.
+
+ select V.customer_id , count(V.visit_id) as count_no_trans
+from Visits V
+left join Transactions T
+on V.visit_id = T.visit_id
+where T.transaction_id IS NULL
+group by V.customer_id;
