e25e0a6bfac2a391d4a376f6d7ae2e1f07a52ee8
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2023-07-23 22:45:26 -0700

    Update monthlyTransaction.sql

--- FTSearch/SQLPractice/monthlyTransaction.sql
+++ FTSearch/SQLPractice/monthlyTransaction.sql
@@ -1,1 +1,11 @@
 
+select DATE_FORMAT(trans_date, '%Y-%m') as month,
+country,
+count(id) as trans_count,
+SUM(CASE when state='approved' then 1 else 0 end ) as approved_count,
+SUM(amount) as trans_total_amount,
+SUM(CASE when state='approved' then amount else 0 end) as approved_total_amount
+
+FROM Transactions
+
+group by month, country;
