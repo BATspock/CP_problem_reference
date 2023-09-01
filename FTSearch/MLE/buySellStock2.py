5ef9fad86c24babc03f61bcca1e97b74bd281add
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2023-09-01 11:25:50 -0700

    Update buySellStock2.py

--- FTSearch/MLE/buySellStock2.py
+++ FTSearch/MLE/buySellStock2.py
@@ -1,1 +1,12 @@
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit, cur = 0, prices[0]
+        
+        for price in prices:
+            if price < cur:
+                cur = price
+            if price - cur > 0:
+                profit+= price - cur
+                cur = price
+        return profit
