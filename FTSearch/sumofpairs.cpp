23e68193535d63dbfc83d3fe318703fe5a3b3723
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2024-01-22 11:50:29 -0800

    Update sumofpairs.cpp

--- FTSearch/sumofpairs.cpp
+++ FTSearch/sumofpairs.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int first = 0;
+        int last = nums.size()-1;
+        int operations = 0;
 
+        if (nums.size()==1){
+           return 0;
+        }
+
+        while(first<last){
+
+            int sum = nums[first]+nums[last];
+
+            if ( sum == k) {
+                operations++;
+                first++;
+                last--;
+            }
+            else if (sum > k){
+                last--;
+            }
+            else {
+                first++;
+            }
+        }
+
+        return operations;
+    }
+};
