5f4ed1092fbd6a1524332f72aa05719a7a194d67
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2024-10-07 07:53:10 -0700

    Update 242IsAnagram.cs

--- C#Practice/242IsAnagram.cs
+++ C#Practice/242IsAnagram.cs
@@ -1,1 +1,13 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) return false;
 
+        Dictionary<char, int> charFrequency = new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; i++) {
+            charFrequency[s[i]] = charFrequency.GetValueOrDefault(s[i], 0) + 1;
+            charFrequency[t[i]] = charFrequency.GetValueOrDefault(t[i], 0) - 1;
+        }
+
+        return charFrequency.All(entry => entry.Value == 0);
+    }
+}
